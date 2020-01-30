9522582e545c05efd0ddfeee40c51860e0eeebe2
1token <zboran@federizer.com>
2020-01-30 19:52:55 +0100

    fts5

--- scripts/sql/sqlite3/email_tb.sql
+++ scripts/sql/sqlite3/email_tb.sql
@@ -5,7 +5,9 @@ DROP TABLE IF EXISTS filter;
 DROP TABLE IF EXISTS label;
 DROP TABLE IF EXISTS recipient;
 DROP TABLE IF EXISTS fts_message;
-DROP TABLE IF EXISTS fts_attachment;
+DROP TABLE IF EXISTS fts_message_recipient;
+DROP TABLE IF EXISTS fts_message_attachment;
+DROP TABLE IF EXISTS fts_message_label;
 VACUUM;
 
 CREATE TABLE timeline_seq
@@ -31,14 +33,14 @@ CREATE TABLE message
     parent_id   binary(16),
     thread_id   binary(16)             NOT NULL,
     fwd         integer(2)             NOT NULL DEFAULT 0,
-    label_ids   text,                            --json
-    "from"      text                   NOT NULL, --json
-    "to"        text,                            --json
-    "cc"        text,                            --json
-    "bcc"       text,                            --json
-    "group"     text,                            --json
-    tags        text,                            --json
-    attachments text,                            --json
+    label_ids   text,                            --json label id
+    "from"      text                   NOT NULL, --json recipient
+    "to"        text,                            --json recipient
+    "cc"        text,                            --json recipient
+    "bcc"       text,                            --json recipient
+    "group"     text,                            --json recipient
+    tags        text,                            --json key/value
+    attachments text,                            --json mimetype, filename, hash
     mimetype    varchar(255),
     subject     varchar(255),
     snippet     varchar(255),
@@ -54,26 +56,16 @@ CREATE TABLE message
 
 CREATE TABLE filter
 (
-    id          binary(16) PRIMARY KEY NOT NULL,
-    owner       varchar(255)           NOT NULL,
-    name        varchar(255)           NOT NULL,
-    criteria    varchar(4096),
-    timeline_id integer(8)             NOT NULL,
-    history_id  integer(8)             NOT NULL,
-    last_stmt   integer(2)             NOT NULL DEFAULT 0,
-    timestamp   integer(4)                      DEFAULT (strftime('%s', DateTime('Now', 'localtime')))
-);
-
-CREATE TABLE label
-(
-    id          binary(16) PRIMARY KEY NOT NULL,
-    owner       varchar(255)           NOT NULL,
-    type        varchar(255)           NOT NULL,
-    name        varchar(255)           NOT NULL,
-    timeline_id integer(8)             NOT NULL,
-    history_id  integer(8)             NOT NULL,
-    last_stmt   integer(2)             NOT NULL DEFAULT 0,
-    timestamp   integer(4)                      DEFAULT (strftime('%s', DateTime('Now', 'localtime')))
+    id            binary(16) PRIMARY KEY NOT NULL,
+    owner         varchar(255)           NOT NULL,
+    name          varchar(255)           NOT NULL,
+    criteria      varchar(4096),
+    label_ids     text, --json label id
+    recipient_ids text, --json recipient id
+    timeline_id   integer(8)             NOT NULL DEFAULT 0,
+    history_id    integer(8)             NOT NULL DEFAULT 0,
+    last_stmt     integer(2)             NOT NULL DEFAULT 0,
+    timestamp     integer(4)                      DEFAULT (strftime('%s', DateTime('Now', 'localtime')))
 );
 
 CREATE TABLE recipient
@@ -82,30 +74,71 @@ CREATE TABLE recipient
     owner         varchar(255)           NOT NULL,
     email_address varchar(255)           NOT NULL,
     display_name  varchar(255),
-    timeline_id   integer(8)             NOT NULL,
-    history_id    integer(8)             NOT NULL,
+    timeline_id   integer(8)             NOT NULL DEFAULT 0,
+    history_id    integer(8)             NOT NULL DEFAULT 0,
     last_stmt     integer(2)             NOT NULL DEFAULT 0,
     timestamp     integer(4)                      DEFAULT (strftime('%s', DateTime('Now', 'localtime')))
 );
 
+CREATE TABLE label
+(
+    id          binary(16) PRIMARY KEY NOT NULL,
+    owner       varchar(255)           NOT NULL,
+    type        integer(2)             NOT NULL DEFAULT 1, --0-system, 1--custom
+    label       varchar(255)           NOT NULL,
+    timeline_id integer(8)             NOT NULL DEFAULT 0,
+    history_id  integer(8)             NOT NULL DEFAULT 0,
+    last_stmt   integer(2)             NOT NULL DEFAULT 0,
+    timestamp   integer(4)                      DEFAULT (strftime('%s', DateTime('Now', 'localtime')))
+);
+
 CREATE VIRTUAL TABLE fts_message USING fts5
 (
-    id UNINDEXED,
+    message_id UNINDEXED,
     subject,
     message,
     "from",
-    "to",
-    "cc",
-    "bcc",
-    "group",
-    tag,
-    label
+    tag
 );
 
-CREATE VIRTUAL TABLE fts_attachment USING fts5
+CREATE VIRTUAL TABLE fts_message_recipient USING fts5
 (
-    id UNINDEXED,
+    message_id UNINDEXED,
+    recipient
+);
+
+CREATE VIRTUAL TABLE fts_message_attachment USING fts5
+(
+    message_id UNINDEXED,
     filename,
     attachment
 );
 
+CREATE VIRTUAL TABLE fts_message_label USING fts5
+(
+    message_id UNINDEXED,
+    label_id UNINDEXED,
+    label
+);
+
+CREATE INDEX idx_message_owner ON message (owner);
+CREATE INDEX idx_message_sent_at ON message (sent_at);
+CREATE INDEX idx_message_received_at ON message (received_at);
+CREATE INDEX idx_message_timeline_id ON message (timeline_id);
+CREATE INDEX idx_message_history_id ON message (history_id);
+CREATE INDEX idx_message_last_stmt ON message (last_stmt);
+
+CREATE INDEX idx_filter_owner ON filter (owner);
+CREATE INDEX idx_filter_timeline_id ON filter (timeline_id);
+CREATE INDEX idx_filter_history_id ON filter (history_id);
+CREATE INDEX idx_filter_last_stmt ON filter (last_stmt);
+
+CREATE INDEX idx_recipient_owner ON recipient (owner);
+CREATE INDEX idx_recipient_timeline_id ON recipient (timeline_id);
+CREATE INDEX idx_recipient_history_id ON recipient (history_id);
+CREATE INDEX idx_recipient_last_stmt ON recipient (last_stmt);
+
+CREATE INDEX idx_label_owner ON label (owner);
+CREATE INDEX idx_label_timeline_id ON label (timeline_id);
+CREATE INDEX idx_label_history_id ON label (history_id);
+CREATE INDEX idx_label_last_stmt ON label (last_stmt);
--- scripts/sql/sqlite3/email_tr.sql
+++ scripts/sql/sqlite3/email_tr.sql
@@ -2,92 +2,92 @@
 
 CREATE TRIGGER IF NOT EXISTS message_after_insert
     AFTER INSERT
-          ON message
-              FOR EACH ROW
+    ON message
+    FOR EACH ROW
 BEGIN
-UPDATE timeline_seq SET num = (num + 1);
-UPDATE history_seq SET num = (num + 1);
-UPDATE message
-SET timeline_id = (SELECT num FROM timeline_seq),
-    history_id  = (SELECT num FROM history_seq),
-    last_stmt   = 0,
-    timestamp   = strftime('%s', DateTime('Now', 'localtime'))
-WHERE id = new.id;
+    UPDATE timeline_seq SET num = (num + 1);
+    UPDATE history_seq SET num = (num + 1);
+    UPDATE message
+    SET timeline_id = (SELECT num FROM timeline_seq),
+        history_id  = (SELECT num FROM history_seq),
+        last_stmt   = 0,
+        timestamp   = strftime('%s', DateTime('Now', 'localtime'))
+    WHERE id = new.id;
 END;
 
 CREATE TRIGGER IF NOT EXISTS message_before_update
     BEFORE UPDATE OF
-    id,
-               owner,
-               parent_id,
-               thread_id,
-               fwd,
-               "from",
-               sent_at,
-               received_at,
-               snoozed_at--,
+        id,
+        owner,
+        parent_id,
+        thread_id,
+        fwd,
+        "from",
+        sent_at,
+        received_at,
+        snoozed_at--,
 --timeline_id,
 --history_id,
 --last_stmt,
 --timestamp
-           ON message
-               FOR EACH ROW
+    ON message
+    FOR EACH ROW
 BEGIN
-SELECT RAISE(ABORT, 'Update not allowed');
+    SELECT RAISE(ABORT, 'Update not allowed');
 END;
 
 CREATE TRIGGER IF NOT EXISTS message_after_update
     AFTER UPDATE OF
-    label_ids,
-              subject,
-              "to",
-              "cc",
-              "bcc",
-              "group",
-              tags,
-              attachments,
-              mimetype,
-              subject,
-              snippet,
-              body_uri
-          ON message
-              FOR EACH ROW
+        label_ids,
+        subject,
+        "to",
+        "cc",
+        "bcc",
+        "group",
+        tags,
+        attachments,
+        mimetype,
+        subject,
+        snippet,
+        body_uri
+    ON message
+    FOR EACH ROW
 BEGIN
-UPDATE timeline_seq SET num = (num + 1);
-UPDATE history_seq SET num = (num + 1);
-UPDATE message
-SET timeline_id = (SELECT num FROM timeline_seq),
-    history_id  = (SELECT num FROM history_seq),
-    last_stmt   = 1,
-    timestamp   = strftime('%s', DateTime('Now', 'localtime'))
-WHERE id = old.id;
+    UPDATE timeline_seq SET num = (num + 1);
+    UPDATE history_seq SET num = (num + 1);
+    UPDATE message
+    SET timeline_id = (SELECT num FROM timeline_seq),
+        history_id  = (SELECT num FROM history_seq),
+        last_stmt   = 1,
+        timestamp   = strftime('%s', DateTime('Now', 'localtime'))
+    WHERE id = old.id;
 END;
 
 -- Mark for delete
 CREATE TRIGGER IF NOT EXISTS message_before_update_delete
     BEFORE UPDATE OF
-    last_stmt
-           ON message
-               FOR EACH ROW
+        last_stmt
+    ON message
+    FOR EACH ROW
 BEGIN
-SELECT RAISE(ABORT, 'Update "last_stmt" not allowed')
-WHERE (new.last_stmt < 0 OR new.last_stmt > 2)
-   OR (old.last_stmt = 2 AND new.last_stmt <> old.last_stmt);
+    SELECT RAISE(ABORT, 'Update "last_stmt" not allowed')
+    WHERE (new.last_stmt < 0 OR new.last_stmt > 2)
+       OR (old.last_stmt = 2 AND new.last_stmt <> old.last_stmt);
 END;
 
 CREATE TRIGGER IF NOT EXISTS message_after_update_delete
     AFTER UPDATE OF
-    last_stmt
-          ON message
-              FOR EACH ROW
-              WHEN new.last_stmt = 2
+        last_stmt
+    ON message
+    FOR EACH ROW
+    WHEN new.last_stmt = 2
 BEGIN
-UPDATE history_seq SET num = (num + 1);
-UPDATE message
-SET history_id = (SELECT num FROM history_seq),
-    last_stmt  = new.last_stmt,
-    timestamp  = strftime('%s', DateTime('Now', 'localtime'))
-WHERE id = old.id;
+    UPDATE history_seq SET num = (num + 1);
+    UPDATE message
+    SET history_id = (SELECT num FROM history_seq),
+        last_stmt  = new.last_stmt,
+        timestamp  = strftime('%s', DateTime('Now', 'localtime'))
+    WHERE id = old.id;
 END;
 
 ------------------------------filter------------------------------
@@ -159,81 +159,6 @@ CREATE TRIGGER IF NOT EXISTS filter_after_update_delete
 BEGIN
     UPDATE history_seq SET num = (num + 1);
     UPDATE filter
-    SET history_id = (SELECT num FROM history_seq),
-        last_stmt  = new.last_stmt,
-        timestamp  = strftime('%s', DateTime('Now', 'localtime'))
-    WHERE id = old.id;
-END;
-
-------------------------------label------------------------------
-
-CREATE TRIGGER IF NOT EXISTS label_after_insert
-    AFTER INSERT
-    ON label
-    FOR EACH ROW
-BEGIN
-    UPDATE timeline_seq SET num = (num + 1);
-    UPDATE history_seq SET num = (num + 1);
-    UPDATE label
-    SET timeline_id = (SELECT num FROM timeline_seq),
-        history_id  = (SELECT num FROM history_seq),
-        last_stmt   = 0,
-        timestamp   = strftime('%s', DateTime('Now', 'localtime'))
-    WHERE id = new.id;
-END;
-
-CREATE TRIGGER IF NOT EXISTS label_before_update
-    BEFORE UPDATE OF
-        id,
-        owner--,
---timeline_id,
---history_id,
---last_stmt,
---timestamp
-    ON label
-    FOR EACH ROW
-BEGIN
-    SELECT RAISE(ABORT, 'Update not allowed');
-END;
-
-CREATE TRIGGER IF NOT EXISTS label_after_update
-    AFTER UPDATE OF
-        type,
-        name
-    ON label
-    FOR EACH ROW
-BEGIN
-    UPDATE timeline_seq SET num = (num + 1);
-    UPDATE history_seq SET num = (num + 1);
-    UPDATE label
-    SET timeline_id = (SELECT num FROM timeline_seq),
-        history_id  = (SELECT num FROM history_seq),
-        last_stmt   = 1,
-        timestamp   = strftime('%s', DateTime('Now', 'localtime'))
-    WHERE id = old.id;
-END;
-
--- Mark for delete
-CREATE TRIGGER IF NOT EXISTS label_before_update_delete
-    BEFORE UPDATE OF
-        last_stmt
-    ON label
-    FOR EACH ROW
-BEGIN
-    SELECT RAISE(ABORT, 'Update "last_stmt" not allowed')
-    WHERE (new.last_stmt < 0 OR new.last_stmt > 2)
-       OR (old.last_stmt = 2 AND new.last_stmt <> old.last_stmt);
-END;
-
-CREATE TRIGGER IF NOT EXISTS label_after_update_delete
-    AFTER UPDATE OF
-        last_stmt
-    ON label
-    FOR EACH ROW
-    WHEN new.last_stmt = 2
-BEGIN
-    UPDATE history_seq SET num = (num + 1);
-    UPDATE label
     SET history_id = (SELECT num FROM history_seq),
         last_stmt  = new.last_stmt,
         timestamp  = strftime('%s', DateTime('Now', 'localtime'))
@@ -315,3 +240,87 @@ BEGIN
     WHERE id = old.id;
 END;
 
+------------------------------label------------------------------
+
+CREATE TRIGGER IF NOT EXISTS label_after_insert
+    AFTER INSERT
+    ON label
+    FOR EACH ROW
+BEGIN
+    UPDATE timeline_seq SET num = (num + 1);
+    UPDATE history_seq SET num = (num + 1);
+    UPDATE label
+    SET timeline_id = (SELECT num FROM timeline_seq),
+        history_id  = (SELECT num FROM history_seq),
+        last_stmt   = 0,
+        timestamp   = strftime('%s', DateTime('Now', 'localtime'))
+    WHERE id = new.id;
+
+    INSERT INTO fts_message_label (message_id, label_id, label)
+    VALUES (NULL, new.id, new.label);
+END;
+
+CREATE TRIGGER IF NOT EXISTS label_before_update
+    BEFORE UPDATE OF
+        id,
+        owner,
+        type--,
+--timeline_id,
+--history_id,
+--last_stmt,
+--timestamp
+    ON label
+    FOR EACH ROW
+BEGIN
+    SELECT RAISE(ABORT, 'Update not allowed');
+END;
+
+CREATE TRIGGER IF NOT EXISTS label_after_update
+    AFTER UPDATE OF
+        label
+    ON label
+    FOR EACH ROW
+BEGIN
+    UPDATE timeline_seq SET num = (num + 1);
+    UPDATE history_seq SET num = (num + 1);
+    UPDATE label
+    SET timeline_id = (SELECT num FROM timeline_seq),
+        history_id  = (SELECT num FROM history_seq),
+        last_stmt   = 1,
+        timestamp   = strftime('%s', DateTime('Now', 'localtime'))
+    WHERE id = old.id;
+
+    UPDATE fts_message_label
+    SET label = new.label
+    WHERE label_id = old.id;
+END;
+
+-- Mark for delete
+CREATE TRIGGER IF NOT EXISTS label_before_update_delete
+    BEFORE UPDATE OF
+        last_stmt
+    ON label
+    FOR EACH ROW
+BEGIN
+    SELECT RAISE(ABORT, 'Update "last_stmt" not allowed')
+    WHERE (new.last_stmt < 0 OR new.last_stmt > 2)
+       OR (old.last_stmt = 2 AND new.last_stmt <> old.last_stmt);
+END;
+
+CREATE TRIGGER IF NOT EXISTS label_after_update_delete
+    AFTER UPDATE OF
+        last_stmt
+    ON label
+    FOR EACH ROW
+    WHEN new.last_stmt = 2
+BEGIN
+    UPDATE history_seq SET num = (num + 1);
+    UPDATE label
+    SET history_id = (SELECT num FROM history_seq),
+        last_stmt  = new.last_stmt,
+        timestamp  = strftime('%s', DateTime('Now', 'localtime'))
+    WHERE id = old.id;
+
+    DELETE FROM fts_message_label
+    WHERE label_id = old.id;
+END;
